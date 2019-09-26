a3aab4a86ae2c669da6a3061599afa255bea599f
Your Name <tfikrie@yahoo.com>
2019-09-25 20:47:36 -0400

    comitte

--- int_rural_sch_repo_app/src/components/Onboarding.js
+++ int_rural_sch_repo_app/src/components/Onboarding.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Login from './Login'
 import SignUp from './SignUp'
+import {Route, NavLink} from 'react-router-dom'
 
 function Onboarding(props) {
 return (
     <div className='Onboarding'>
-        <Login />
-        <SignUp />
+        <NavLink to='/login/signin'>signin </NavLink>
+        <NavLink to='/login/login'>signup </NavLink>
+        <Route exact path='/login/signin' component={Login} />
+        <Route exact path='/login/login' component={SignUp } />
     </div>
 )
 }
