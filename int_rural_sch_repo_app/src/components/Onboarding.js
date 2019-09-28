6f5ec734a140e3b68c6feef7174fe8e9f7b2eb4d
Your Name <tfikrie@yahoo.com>
2019-09-28 12:44:35 -0400

    react sign in page separated

--- int_rural_sch_repo_app/src/components/Onboarding.js
+++ int_rural_sch_repo_app/src/components/Onboarding.js
@@ -6,10 +6,10 @@ import {Route, NavLink} from 'react-router-dom'
 function Onboarding(props) {
 return (
     <div className='Onboarding'>
-        <NavLink to='/login/signin'>signin </NavLink>
-        <NavLink to='/login/login'>signup </NavLink>
+        <NavLink to='/login/signin' className='signinup'>Signin </NavLink>
+        <NavLink to='/login/signup' className='signinup'>Signup </NavLink>
         <Route exact path='/login/signin' component={Login} />
-        <Route exact path='/login/login' component={SignUp } />
+        <Route exact path='/login/signup' component={SignUp } />
     </div>
 )
 }
